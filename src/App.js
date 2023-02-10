33892792d7b9d68d52054269db0aafa5b7be8d6a
INSEOP KIM <inseop0813@gmail.com>
2023-02-10 13:50:19 +0900

    theme settings & use apollo reative variables

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
+/* routes */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import routes from "./routes";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
 import SignUp from "./screens/SignUp";
 
-import routes from "./routes";
+/* styled-component */
+import { ThemeProvider } from "styled-components";
+
+/* apollo - reactive variables */
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar, darkModeVar } from "./apollo";
+
+const lightTheme = {
+  fontColor: "#c2c2c2",
+  bgColor: "lightgray",
+};
+
+const darkTheme = {
+  fontColor: "lightgray",
+  bgColor: "#c2c2c2",
+};
 
 function App() {
-  const isLoggedIn = false;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
 
   return (
-    <Router>
-      <Routes>
-        <Route path={routes.home} element={isLoggedIn ? <Home /> : <Login />} />
-        <Route path={routes.profile} element={<Profile />} />
-        <Route path={routes.signUp} element={isLoggedIn ? null : <SignUp />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Routes>
+          <Route
+            path={routes.home}
+            element={isLoggedIn ? <Home /> : <Login />}
+          />
+          <Route path={routes.profile} element={<Profile />} />
+          <Route
+            path={routes.signUp}
+            element={isLoggedIn ? null : <SignUp />}
+          />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,31 @@
+import styled from "styled-components";
+
 function Login() {
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
+
+  const Wrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    padding: 4em;
+    background: papaywhip;
+  `;
+
   return (
     <div>
-      <div>
-        <form>
-          <input type="email" />
-          <input type="password " />
-          <input type="submit" />
-        </form>
-      </div>
+      <Wrapper>
+        <Title>인스타그램</Title>
+        <div>
+          <form>
+            <input type="email" />
+            <input type="password " />
+            <input type="submit" />
+          </form>
+        </div>
+      </Wrapper>
     </div>
   );
 }
